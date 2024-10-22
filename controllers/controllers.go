3468317d959e203493a49482468f7f7d9ef4df08
Patosya <coolpato@yandex.ru>
2024-10-22 15:10:41 +0300

    added controllers and start making main

--- controllers/controllers.go
+++ controllers/controllers.go
@@ -2,12 +2,39 @@ package controllers
 
 import (
 	"context"
+	"log"
 	"persogo/models"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/v2/mongo"
 )
 
 func CreateEmployee(collection *mongo.Collection, employee models.Employee) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	employee.ID = primitive.NewObjectID()
+	_, err := collection.InsertOne(ctx, employee)
+	if err != nil {
+		log.Println("Ошибка при создании сотрудника!", err)
+		return err
+	}
+	log.Println("Сотрудник добавлен!")
+	return nil
+}
+
+func GetEmployee(collection *mongo.Collection) ([]models.Employee, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	cursor, err := collection.Find(ctx, bson.M{})
+	if err != nil {
+		return nil, err
+	}
+	var employees []models.Employee
+	if err = cursor.All(ctx, &employees); err != nil {
+		return nil, err
+	}
+	return employees, nil
 }
--- src/main.go
+++ src/main.go
@@ -1,1 +1,15 @@
 package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/employee", employeesHandler)
+	log.Fatal(http.ListenAndServe(":8787", nil))
+}
+
+func employeesHandler(w http.ResponseWriter, r *http.Request) {
+
+}
